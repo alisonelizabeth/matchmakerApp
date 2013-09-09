559098adb91b2db4c330054b7463ad9b75c006e7
Tyler Phillips <tylerfphillips@gmail.com>
2013-09-09 00:49:22 -0400

    Added jasmine files and jasmine tests, updated mocha tests but can be ignored

--- app/spec/jasmine-test.js
+++ app/spec/jasmine-test.js
@@ -1,11 +1,104 @@
 describe('returns an object that', function(){
-	it('damages another fighter with kick', function(){
-		var threeHP = threeFighter.hp
-		var oneDamage = oneFighter.damage
+	it('checks for validity and returns error', function(){
+		var valid = true;
+			$('#name').removeClass("highlight")
+			$('.message').html('<p> </p>')
+			$('#name').each(function(){
+				if ($(this).val() == "") {
+					valid = false
+					$(this).addClass("highlight")
+					$('.message').html('<p>*Please enter your name.</p>')
+				}
+			})
+			 return valid
 
-		oneFighter.kick(threeFighter)
-
-		expect(threeFighter.hp).toBe(threeHP - 120)
+		expect("valid").toBe(true)
 
 		});
 	});
+
+describe('returns an object that', function(){
+	it('checks selected gender and returns object matching selected gender', function(){
+		selected = {
+		genderInput: $("#gender").val(), 
+		ageInput: $("#age-range").val(),
+		personalityInput: $("#personality").val()
+	}
+
+	matches = celebrity.filter(function(obj) {
+		return	obj.gender 		!== selected.genderInput &&
+				obj.ageRange 	=== selected.ageInput &&
+				obj.personality === selected.personalityInput; 
+	});
+	
+
+		expect(selected.genderInput).toBe("Male")
+
+		});
+	it('checks selected personality and returns object matching selected personality', function(){
+		selected = {
+		personalityInput: $("#personality").val()
+	}
+
+	matches = celebrity.filter(function(obj) {
+		return obj.personality === selected.personalityInput; 
+	});
+	
+
+		expect(selected.personalityInput).toBe("funny")
+
+		});
+	it('checks selected age range and returns object matching selected age range', function(){
+		selected = {
+		ageInput: $("#age-range").val()
+	}
+
+	matches = celebrity.filter(function(obj) {
+		return obj.ageRange === selected.ageInput; 
+	});
+	
+
+		expect(selected.ageInput).toBe("20-35")
+
+		});
+});
+
+describe('returns an object that', function(){
+	it('is the celebrity that matches specific attributes', function(){
+			var celebrity = [
+			{name: "Taylor Swift",
+			name2: "<b>Taylor Swift</b>" + "<br>",
+			gender: "Female",
+			ageRange: "20-35",
+			personality: "romantic",
+			image: "<img src='images/Swift.jpg' />"},
+
+			{name: "Kristen Stewart",
+			name2: "<b>Kristen Stewart</b>" + "<br>",
+			gender: "Female",
+			ageRange: "20-35",
+			personality: "serious",
+			image: "<img src='images/Kristen.jpg' />" }
+			]
+
+			var TaylorSwift = celebrity[0]
+			var KristenStewart = celebrity[1]
+
+		
+		expect(celebrity[0]).toBe(TaylorSwift)
+
+		});
+	});
+
+describe('returns an object that', function(){
+	it('is the string, or name of the user, which the user specifies', function(){
+			var name = $("#name").val();
+
+		
+		expect(name).toBe("")
+
+		});
+	});
+
+	
+
--- test/spec/test.js
+++ test/spec/test.js
@@ -13,3 +13,69 @@
         });
     });
 })();
+
+(function () {
+    describe('Form validation', function () {
+        describe('when name is left empty', function () {
+            it('should return an error message', function () {
+				var valid = true;
+							$('#name').removeClass("highlight")
+							$('.message').html('<p> </p>')
+							$('#name').each(function(){
+								if ($(this).val() == "") {
+									valid = false
+									$(this).addClass("highlight")
+									$('.message').html('<p>Please enter your name.</p>')
+								}
+							})
+							 return valid
+						
+            });
+        });
+    });
+})();
+
+(function () {
+    describe('Celebrity is defined', function () {
+        describe('with corresponding attributes', function () {
+            it('name, gender, age, personality, image', function () {
+var celebrity = [
+    //female celebs 
+{name: "Taylor Swift",
+gender: "Female",
+ageRange: "20-35",
+personality: "romantic",
+image: "<img src='images/Swift.jpg' />"}]
+            });
+        });
+    });
+})();
+
+
+(function () {
+    describe('Object is returned', function () {
+        describe('that is the celebrity name that corresponds to', function () {
+            it('user input gender, age, and personality', function () {
+					
+			
+      // your code here 
+      var name = ("#name").val();
+	("#preview-text").text(name + ', your celebrity match is:')
+    
+
+            });
+        });
+    });
+})();
+
+
+(function () {
+    describe('Celebrity is defined', function () {
+        describe('with corresponding attributes', function () {
+            it('name, gender, age, personality, image', function () {
+var button = $(".preview-button").html("<button type='button' class='btn btn-danger'>Not satisfied?</button>")
+
+            });
+        });
+    });
+})();
